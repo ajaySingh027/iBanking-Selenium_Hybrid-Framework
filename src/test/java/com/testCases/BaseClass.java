861ec13ccfea1902925ea09f1be2ccbd8d37d582
Ajay Singh <a.pratap027@gmail.com>
2020-04-15 21:42:10 -0400

    Code update for paramaterised browser input from xml

--- src/test/java/com/testCases/BaseClass.java
+++ src/test/java/com/testCases/BaseClass.java
@@ -1,13 +1,19 @@
 package com.testCases;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import com.Utilities.ReadConfig;
 
@@ -21,16 +27,30 @@ public class BaseClass {
 	public static Logger logger;
 	
 	
+	@Parameters("browser")
 	@BeforeClass
-	public void setup() {
-		
-//		System.setProperty("webdriver.gecko.driver", "/Users/sneharoy/eclipse-workspace/InetBankingV1/Drivers/geckodriver");
-		System.setProperty("webdriver.gecko.driver", readconfig.getFirefoxPath());
-		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	public void setup(String br) {
 		
 		logger = Logger.getLogger("ebanking");
 		PropertyConfigurator.configure("log4j.properties");
+		
+		if(br.contentEquals("chrome"))
+		{
+			
+		}
+		else if(br.equals("firefox"))
+		{
+//			System.setProperty("webdriver.gecko.driver", "/Users/sneharoy/eclipse-workspace/InetBankingV1/Drivers/geckodriver");
+			System.setProperty("webdriver.gecko.driver", readconfig.getFirefoxPath());
+			driver = new FirefoxDriver();
+		}
+		else if(br.equals("ie"))
+		{
+			
+		}
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
 		
 	}
 
@@ -39,5 +59,14 @@ public class BaseClass {
 	public void tearDown() {
 		
 		driver.quit();
+	}
+	
+	
+	public void captureScreen(WebDriver driver, String tname) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File target = new File(System.getProperty("user.dir") + "/Screenshots" + tname + ".png");
+		FileUtils.copyFile(source, target);
+		System.out.println("-- Screenshot taken --");
 	}
 }
--- src/test/java/com/testCases/TC_LoginTest_001.java
+++ src/test/java/com/testCases/TC_LoginTest_001.java
@@ -1,6 +1,7 @@
 package com.testCases;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
@@ -15,11 +16,25 @@ public class TC_LoginTest_001 extends BaseClass {
 				
 			LoginPage logintest = PageFactory.initElements(driver, LoginPage.class);
 			driver.get(URL);
+			logger.info("-- Starting the login--");
+			
 			logintest.getuserId().sendKeys(userid);
 			logintest.getpassword().sendKeys(password);
 			logintest.getloginbtn().click();
 			logger.info("-- Login completed here-- ");
-			Thread.sleep(2000);
+			Thread.sleep(1000);
+			
+			if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
+			{
+				Assert.assertTrue(true);
+				logger.info("--Login test passed--");
+			}
+			else
+			{
+				captureScreen(driver, "logintest");
+				Assert.assertTrue(false);
+				logger.info("--Login test Failed--");
+			}
 
 		} catch(Exception e) {
 			e.printStackTrace();
